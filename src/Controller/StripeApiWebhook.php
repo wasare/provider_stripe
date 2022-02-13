fd6399736750bf6daf92df7313f623d0fdf0eb6b
Wanderson Reis <wsreis@gmail.com>
2022-02-13 13:57:52 -0300

    Improve coding standards

--- src/Controller/StripeApiWebhook.php
+++ src/Controller/StripeApiWebhook.php
@@ -74,9 +74,9 @@ class StripeApiWebhook extends ControllerBase {
     }
 
     if ($config->get('log_webhooks')) {
-       /** @var \Drupal\Core\Logger\LoggerChannelInterface $logger */
-       $logger = $this->getLogger('provider_stripe');
-       $logger->info("Stripe webhook received event:\n @event", ['@event' => (string)$event]);
+      /** @var \Drupal\Core\Logger\LoggerChannelInterface $logger */
+      $logger = $this->getLogger('provider_stripe');
+      $logger->info("Stripe webhook received event:\n @event", ['@event' => (string) $event]);
     }
 
     // Dispatch the webhook event.
